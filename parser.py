9d86095a141dbb202083acd17e2c3a3ac1f7e190
José Marcel Mendes Nogueira <marcelnogueirasgs@gmail.com>
2019-10-15 15:21:36 -0300

    Fix parser.py

--- parser.py
+++ parser.py
@@ -68,4 +68,84 @@ class Parser():
         def program(p):
             return p[0]
 
-        
+        @self.pg.production('comando : decvar PONTOEVIRGULA')
+        @self.pg.production('comando : decatrib PONTOEVIRGULA')
+        @self.pg.production('comando : decprint PONTOEVIRGULA')
+        @self.pg.production('comando : decread PONTOEVIRGULA')
+        @self.pg.production('comando : decif PONTOEVIRGULA')
+        @self.pg.production('comando : decfor PONTOEVIRGULA')
+        @self.pg.production('comando : decwhile PONTOEVIRGULA')
+        def comando(p):
+            return p[0]
+
+        @self.pg.production('comando : comando comando')
+        def comandos(p):
+            return p[0] + p[1]
+
+        @self.pg.production('operacao : oprelacional PONTOEVIRGULA')
+        @self.pg.production('operacao : oplogico PONTOEVIRGULA')
+        @self.pg.production('operacao : oparitmetico PONTOEVIRGULA')
+        def operacao(p):
+            return p[0]
+
+        @self.pg.production('oparitmetico : IDENT SOMA IDENT')
+        @self.pg.production('oparitmetico : IDENT SUBTRACAO IDENT')
+        @self.pg.production('oparitmetico : IDENT MULTIPLICACAO IDENT')
+        @self.pg.production('oparitmetico : IDENT DIVISAO IDENT')
+        @self.pg.production('oparitmetico : IDENT RESTO IDENT')
+        def oparitmetico(p):
+            left = p[0]
+            right = p[2]
+            op = p[1]
+            if op.gettokentype() == 'SOMA':
+                return #funcao ast Sum(left, right)
+            elif op.gettokentype() == 'SUBTRACAO':
+                return #funcao ast Sub(left, right)
+            elif op.gettokentype() == 'MULTIPLICACAO':
+                return #funcao ast Sub(left, right)
+            elif op.gettokentype() == 'DIVISAO':
+                return #funcao ast Sub(left, right)
+            elif op.gettokentype() == 'RESTO':
+                return #funcao ast Sub(left, right)
+        '''
+oparitimetico: 
+	"ident [( SOMA |
+		  SUBTRACAO | 
+		  MULTIPLICACAO | 
+		  DIVISAO | 
+		  RESTO )] ident"
+
+oplogico:
+	"(ident [( AND | OR )] ident)|( NOT ident )"
+
+oprelacional:
+	"ident [( MENOR | MAIOR | MENOROUIGUAL | MAIOROUIGUAL | IGUAL | DIFERENTE)] ident"
+
+
+for:
+	FOR ABREPARENTESES ident PONTOEVIRGULA operacao PONTOEVIRGULA atribuicao FECHAPARENTESES INICIO BLOCO comando|operacao FIMBLOCO
+
+while:	
+	WHILE ABREPARENTESES operacao FECHAPARENTESES INICIOBLOCO comando|operacao
+FIMBLOCO
+
+if:
+	IF ABREPARENTESES operacao FECHAPARENTESES INICIOBLOCO comando FIMBLOCO ELSE INICIOBLOCO comando|operacao FIMBLOCO
+
+read:
+ 	"READ ABREPARENTESES ident FECHAPARENTESES"
+
+print:
+	"PRINT ABREPARENTESES [(NUMERO* | CARACTER* | ident | oparitmetico)] FECHAPARENTESES "
+	
+decatrib:
+	"(INT | FLOAT | CHAR) ident ATRIBUICAO (NUMERO | REAL | ABREASPAS CARACTER FECHAASPAS’ |ident)"
+
+ident:
+	"CARACTER[(NUMERO|CARACTER)]*"
+
+decvar:
+	"( INT | FLOAT | CHAR ) ident"
+
+
+        '''
